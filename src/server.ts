5086b2887882e26850faba6aa03cedadeb3a9ff8
RAWAT DIKSHA <ite001451@athuman.com>
2023-06-12 15:25:11 +0900

    changes in server.ts

--- src/server.ts
+++ src/server.ts
@@ -27,7 +27,7 @@ const mongoDBstore = new MongoDBStore({
 app.use(
   cors({
     allowedHeaders: ['Content-Type','Authorization'],
-    origin: "http://localhost:3000",
+    origin: "https://latestfrontend.vercel.app",
     methods: ["GET","POST","PUT","DELETE","OPTIONS","HEAD"],
     credentials: true
   })
